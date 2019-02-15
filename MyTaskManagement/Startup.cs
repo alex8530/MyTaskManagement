877c2ef348b8f98de7ca6c580bb68788e3e78918
Alex <Aliabuharb8530@gmil.com>
2019-02-15 15:31:10 +0200

    Fix Bug in startup class

--- MyTaskManagement/Startup.cs
+++ MyTaskManagement/Startup.cs
@@ -13,61 +13,9 @@ namespace MyTaskManagement
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            createMyRolesandUsers();
+           
         }
 
-        private void createMyRolesandUsers()
-        {
-            ApplicationDbContext context = new ApplicationDbContext();
-
-            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-          
-            if (!RoleManager.RoleExists("Admin"))
-            {
-
-                // first we create Admin role   
-                var role = new IdentityRole();
-                role.Name = "Admin";
-                RoleManager.Create(role);
-
-             
-            }
-            // creating Creating Manager role   
-            if (!RoleManager.RoleExists("ProjectManager"))
-            {
-                var role = new IdentityRole();
-                role.Name = "Manager";
-                RoleManager.Create(role);
-
-            }
-
-            // creating Creating Employee role   
-            if (!RoleManager.RoleExists("Employee"))
-            {
-                var role = new  IdentityRole();
-                role.Name = "Employee";
-                RoleManager.Create(role);
-
-            }
-
-            //Here we create a Admin super user who will maintain the website                 
-
-            var user = new ApplicationUser();
-            user.UserName = "superadmin";
-            user.Email = "superadmin@superadmin.com";
-
-            string userPWD = "123123";
-
-            var chkUser = UserManager.Create(user, userPWD);
-
-            //Add default User to Role Admin  
-            if (chkUser.Succeeded)
-            {
-                var result1 = UserManager.AddToRole(user.Id, "Admin");
-
-            }
-
-        }
+        
     }
 }
